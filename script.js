d9dd47915065725ec2c9ab1299da5335724debaa
JohanFre <70636307+JohanFre@users.noreply.github.com>
2020-11-02 14:22:45 +0100

    patch 1.0

--- script.js
+++ script.js
@@ -1,51 +1,118 @@
-// Get peer id without hash from URL.
-
-const myPeerId = location.hash.slice(1);
-
-// Connect to Peer server.
-peer = new Peer(myPeerId, {
-  host: "glajan.com",
-  port: 8443,
-  path: "/myapp",
-  secure: true,
-});
-
-// Print peer id on connection "open" event.
-peer.on("open", (id) => {
-  const myPeerIdEl = document.querySelector(".my-peer-id");
-  myPeerIdEl.innerText = myPeerId;
-});
-
-// Refreshbutton shows others ID's and creates them in a list with a connect-button.
-
-const listPeersButtonEl = document.querySelector(".list-all-peers-button");
-
-listPeersButtonEl.addEventListener("click", () => {
-  const ulEl = document.createElement("ul");
-  const peersList = document.querySelector(".peers");
-  peersList.firstChild && peersList.firstChild.remove();
-  peer.listAllPeers((peers) => {
-    peers
-      .filter((p) => p !== myPeerId)
-      .map((peers) => {
-        const liEl = document.createElement("li");
-        const peersButton = document.createElement("button");
-        peersButton.innerText = peers;
-        peersButton.classList.add("connect-button");
-        peersButton.classList.add("peerId-${peer}");
-        liEl.appendChild(peersButton);
-        ulEl.appendChild(liEl);
-      });
-    peersList.appendChild(ulEl);
+(function () {
+  let dataConnection = null;
+  const peersEl = document.querySelector(".peers");
+  const sendButtonEl = document.querySelector(".send-new-message-button");
+  const newMessageEl = document.querySelector(".new-message");
+  // get peer id from URL (no hash)
+  const myPeerId = location.hash.slice(1);
+  // connect to peer server
+  let peer = new Peer(myPeerId, {
+    host: "glajan.com",
+    port: 8443,
+    path: "/myapp",
+    secure: true,
+    config: {
+      iceServers: [
+        { urls: ["stun:eu-turn7.xirsys.com"] },
+        {
+          username:
+            "1FOoA8xKVaXLjpEXov-qcWt37kFZol89r0FA_7Uu_bX89psvi8IjK3tmEPAHf8EeAAAAAF9NXWZnbGFqYW4=",
+          credential: "83d7389e-ebc8-11ea-a8ee-0242ac140004",
+          urls: [
+            "turn:eu-turn7.xirsys.com:80?transport=udp",
+            "turn:eu-turn7.xirsys.com:3478?transport=udp",
+            "turn:eu-turn7.xirsys.com:80?transport=tcp",
+            "turn:eu-turn7.xirsys.com:3478?transport=tcp",
+            "turns:eu-turn7.xirsys.com:443?transport=tcp",
+            "turns:eu-turn7.xirsys.com:5349?transport=tcp",
+          ],
+        },
+      ],
+    },
   });
-});
+  // print peer id on connection "open" event.
+  peer.on("open", (id) => {
+    const myPeerIdEl = document.querySelector(".my-peer-id");
+    myPeerIdEl.innerText = id;
+  });
+  // error message if there is an error
+  peer.on("error", (errorMessage) => {
+    console.error(errorMessage);
+  });
 
-// button for sending text message
+  // On incoming connectoin.
+  peer.on("connection", (connection) => {
+    console.log("connection");
 
-// window auto refreshing chatmessages
+    dataConnection = connection;
 
-// video window streaming own webcam
+    dataConnection.on("data", (textMessage) => {
+      console.log(dataConnection.peer + ": " + textMessage);
+    });
 
-// video window recieving other ID's webcam
+    const event = new CustomEvent("peer-changed", { detail: connection.peer });
+    document.dispatchEvent(event);
+  });
+  // event listener for click "refresh list"
+  const refreshPeersButtonEl = document.querySelector(".list-all-peers-button");
+  refreshPeersButtonEl.addEventListener("click", (e) => {
+    peer.listAllPeers((peers) => {
+      // add peers to html document
+      const peersList = peers
+        // filter out our own name
+        .filter((peerId) => peerId !== peer._id)
+        // loop through all peers and print them as buttons in a list
+        .map((peer) => {
+          return `<li>
+                      <button class="connect-button peerId-${peer}">${peer}</button>
+                          </li>`;
+        })
+        .join("");
+      peersEl.innerHTML = `<ul>${peersList}</ul>`;
+    });
+  });
+  // event listeneer for click on peer button
+  peersEl.addEventListener("click", (event) => {
+    if (!event.target.classList.contains("connect-button"));
+    // get peerId from button element
+    const theirPeerId = event.target.innerText;
+    // close existing connection
+    dataConnection && dataConnection.close();
+    // connect to peer
+    dataConnection = peer.connect(theirPeerId);
 
-// button for starting video confrance
+    dataConnection.on("data", (textMessage) => {
+      console.log(dataConnection.peer + ": " + textMessage);
+    });
+
+    dataConnection.on("open", () => {
+      // dispatch Custom event with connected peer id
+      const event = new CustomEvent("peer-changed", {
+        detail: theirPeerId,
+      });
+      document.dispatchEvent(event);
+    });
+  });
+  // listen for custom event 'peer changed''
+  document.addEventListener("peer-changed", (e) => {
+    const peerId = e.detail;
+    // get clicked button
+    const connectButtonEl = document.querySelector(
+      `.connect-button.peerId-${peerId}`
+    );
+    //remove class from connected
+    document.querySelectorAll(".connect-button.connected").forEach((button) => {
+      button.classList.remove("connected");
+    });
+    // add class 'connected to clicked connectbutton
+    connectButtonEl.classList.add("connected");
+    console.log(peerId);
+  });
+
+  // Event listener for click on"send".
+  sendButtonEl.addEventListener("click", () => {
+    if (!dataConnection) return;
+    // Get new message from text input.
+    dataConnection.send(newMessageEl.value);
+  });
+})();
